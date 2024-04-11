09a09da0dba5a382095de9c5f0dd55762a6c279d
vianho <silviana.h016@gmail.com>
2024-04-12 00:14:08 +0800

    chore: project created with projen

---
+++ .projenrc.py
@@ -0,0 +1,12 @@
+from apidays24pj import PythonProject
+
+project = PythonProject(
+    author_email="silviana.h016@gmail.com",
+    author_name="vianho",
+    dev_deps=["@vianho/apidays24pj"],
+    module_name="noodles",
+    name="noodles",
+    version="0.1.0",
+)
+
+project.synth()
