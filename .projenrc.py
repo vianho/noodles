6fe47e38f4c6f53fca5efd86b92cf3bc38c4b0b4
vianho <silviana.h016@gmail.com>
2024-04-16 00:47:48 +0800

    chore:version pin apidays24pj package

--- .projenrc.py
+++ .projenrc.py
@@ -3,7 +3,7 @@ from apidays24pj import PythonProject
 project = PythonProject(
     author_email="silviana.h016@gmail.com",
     author_name="vianho",
-    dev_deps=["@vianho/apidays24pj"],
+    dev_deps=["apidays24pj@0.0.14"],
     module_name="noodles",
     name="noodles",
     version="0.1.0",
